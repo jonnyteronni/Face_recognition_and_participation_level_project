42af6de614a5ebfd9be7057eb39abf4536bc56d1
jonnyteronni <antero.soeiro@gmail.com>
2020-08-21 15:01:48 +0100

    Update scripts with csv saves

--- Face_recognition_2.py
+++ Face_recognition_2.py
@@ -4,7 +4,7 @@ import numpy as np
 from mss import mss
 import sys
 import os
-
+import time
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
@@ -22,15 +22,35 @@ import os
 
 # choose between webcam('w'), part of screen_part('sp'), fullscreen('fs') or video('v')
 
-
+# -------DASHBOARD--------
 type_of_input = 'sp'
+
+# hog for cpu, cnn for GPU
+MODEL_LOCATION = 'hog'
+
+# large or small. Small is faster but less accurate
+MODEL_ENCODING = 'large'
+
+# How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+# Only integers
+NUM_JITTERS_ENCODING = 1
+
+# How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
+TOLERANCE_RECOGNITION = 0.6
+
+# Resize of 
+resize_value = 1
+
+# -------------
 
 # Check what is the OS running
 
 if os.name == 'Linux':
     print('Yeiii Linux is running here!')
 else:
-    sct=mss()
+    print('Recommend using linux to run this script.')
+
+sct=mss()    
 
 
 if type_of_input == 'w':
@@ -42,7 +62,7 @@ if type_of_input == 'w':
     
 elif type_of_input == 'sp':
     # with screen_part
-    monitor = {"top": 300, "left": 0, "width": 1300, "height": 800}
+    monitor = {"top": 300, "left": 0, "width": 1000, "height": 800}
 elif type_of_input == 'fs':
     # with fullscreen
     monitor = sct.monitors[2]
@@ -72,7 +92,7 @@ for name in os.listdir(KNOWN_FACES_DIR):
         
         # for the faces not detected we need an exception
         try:
-            encoding = face_recognition.face_encodings(image)[0]
+            encoding = face_recognition.face_encodings(image,num_jitters = 1)[0]
         except IndexError as error_message:
             print(error_message,': Face not found')
             
@@ -84,46 +104,10 @@ for name in os.listdir(KNOWN_FACES_DIR):
         known_faces.append(encoding)
         known_names.append(name)
 
+# known_faces = np.genfromtxt('models/known_faces_model.csv',delimiter=',')
+# known_names= np.genfromtxt('models/known_names_model.csv',delimiter=',')
 
 
-
-# # Encoding single samples ------------------------------
-
-# # Load a sample picture and learn how to recognize it.
-# inverno_image = face_recognition.load_image_file("face_recognition/inverno.jpg")
-# inverno_face_encoding = face_recognition.face_encodings(inverno_image)[0]
-
-# # Load a second sample picture and learn how to recognize it.
-# antero_image = face_recognition.load_image_file("face_recognition/antero.JPG")
-# antero_face_encoding = face_recognition.face_encodings(antero_image)[0]
-
-# jose_image = face_recognition.load_image_file("face_recognition/Jose.PNG")
-# jose_face_encoding = face_recognition.face_encodings(jose_image)[0]
-
-# guillaume_image = face_recognition.load_image_file("face_recognition/guillaume.png")
-# guillaume_face_encoding = face_recognition.face_encodings(guillaume_image)[0]
-
-# pedro_image = face_recognition.load_image_file("face_recognition/pedro.png")
-# pedro_face_encoding = face_recognition.face_encodings(pedro_image)[0]
-
-# #------------------------------------------------
-
-# # Create arrays of known face encodings and their names
-# known_faces = [
-#     inverno_face_encoding,
-#     antero_face_encoding,
-#     guillaume_face_encoding,
-#     jose_face_encoding
-#     pedro_face_encoding
-    
-# ]
-# known_names = [
-#     "Inverno",
-#     "Antero",
-#     "Guilhaume",
-#     "Jose"
-#     "Pedro",
-# ]
 
 # Initialize some variables
 face_locations = []
@@ -134,6 +118,8 @@ process_this_frame = True
 while True:
     # Grab a single frame of video
     # ret, frame = video_capture.read()
+    
+    initial_frame = time.time()
     
     if type_of_input == 'w' or type_of_input=='v':
         ret, frame = webcam.read()
@@ -153,7 +139,7 @@ while True:
 
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -161,13 +147,13 @@ while True:
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_locations = face_recognition.face_locations(rgb_small_frame,model = MODEL_LOCATION)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations,num_jitters = NUM_JITTERS_ENCODING, model = MODEL_ENCODING)
         
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_faces, face_encoding)
+            matches = face_recognition.compare_faces(known_faces, face_encoding, tolerance=TOLERANCE_RECOGNITION)
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
@@ -189,10 +175,10 @@ while True:
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        top *= 1
+        right *= 1
+        bottom *= 1
+        left *= 1
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -209,6 +195,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+    print(time.time() - initial_frame)
 
 # Release handle to the webcam
 webcam.release()

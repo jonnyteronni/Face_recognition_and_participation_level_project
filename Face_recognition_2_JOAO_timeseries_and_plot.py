b17d74ade5b8b04cf75bc6b4525d045862d0713d
jinverno <65239409+jinverno@users.noreply.github.com>
2020-08-24 02:45:22 +0100

    timeseries and ploting gif

--- Face_recognition_2_JOAO_timeseries_and_plot.py
+++ Face_recognition_2_JOAO_timeseries_and_plot.py
@@ -24,7 +24,7 @@ import time
 # choose between webcam('w'), part of screen_part('sp'), fullscreen('fs') or video('v')
 
 # -------DASHBOARD--------
-type_of_input = 'w'
+type_of_input = 'fs'
 
 # hog for cpu, cnn for GPU
 MODEL_LOCATION = 'hog'
@@ -243,30 +243,71 @@ if type_of_input == 'w':
     webcam.release()
 cv2.destroyAllWindows()
 
-# time_p=0
-# time_p=[i[1]+i[1][x-1] for x,i in enumerate(timeseries[1:])]
-# print(time_p)
 
-# import pandas as pd
-# timeseries_df=pd.DataFrame(timeseries)
-# for i in timeseries_df[0].unique():
-#     timeseries_df[i]=timeseries_df[timeseries_df[0]==i][1].cumsum()
-# timeseries_df.fillna(0,inplace=True)
-# timeseries_df.drop([1],axis=1,inplace=True)
 
-# import matplotlib as plt
-# import numpy as np
+#Creating timeseries dataframe and cum sum
+import pandas as pd
+timeseries_df=pd.DataFrame(timeseries)
+for i in timeseries_df[0].unique():
+    timeseries_df[i]=timeseries_df[timeseries_df[0]==i][1]
+    timeseries_df[i].fillna(0,inplace=True)
+    timeseries_df[i]=timeseries_df[i].cumsum()
 
-# plt.rcdefaults()
-# fig, ax = plt.subplots()
+timeseries_df.drop([1],axis=1,inplace=True)
 
-# order=timeseries_df.max()[1:].sort_values().keys().tolist()
-# y_pos=np.arange(len(order))
-# x_speak=[]
 
-# for i in timeseries_df.iterrows():
-#     for ii in np.arange(len(order)):
-#         x_speak.append(i[1][(order[ii])])
-#     ax.barh(y_pos, x_speak, align='center')
-#     plt.show()
+#Ploting timeseries of facetime
+import matplotlib.pyplot as plt 
+import numpy as np
+import imageio
+import os
+
+
+order=timeseries_df.max()[1:].sort_values(ascending=False).keys().tolist()
+y_pos=np.arange(len(order))
+x_speak=[]
+ims=0
+
+for i in timeseries_df.iterrows():
+    for ii in np.arange(len(order)):
+        x_speak.append(i[1][(order[ii])])
+    plt.figure(figsize=(15,2))
+    plt.barh(y_pos, x_speak, align='center',color="slategrey")
+    plt.yticks(y_pos,order)
+    plt.xlim(xmax=int(1.05*max(timeseries_df.max()[1:].values)))
     
+    x_speak=[]
+    ims+=1
+    
+#Saving the frames
+    plt.savefig('./gif/all/'+str(ims)+'.png',dpi=150)
+    plt.show()
+    
+
+#Creating Gif
+folder = './gif/all' 
+files = [f"{folder}\\{file}" for file in os.listdir(folder)]
+
+images = [imageio.imread(file) for file in files]
+imageio.mimwrite('./gif/movie.gif', images, fps=5)
+
+    # Parameters for saving
+    # ---------------------
+    # loop : int
+    #     The number of iterations. Default 0 (meaning loop indefinitely).
+    # duration : {float, list}
+    #     The duration (in seconds) of each frame. Either specify one value
+    #     that is used for all frames, or one value for each frame.
+    #     Note that in the GIF format the duration/delay is expressed in
+    #     hundredths of a second, which limits the precision of the duration.
+    # fps : float
+    #     The number of frames per second. If duration is not given, the
+    #     duration for each frame is set to 1/fps. Default 10.
+    # palettesize : int
+    #     The number of colors to quantize the image to. Is rounded to
+    #     the nearest power of two. Default 256.
+    # subrectangles : bool
+    #     If True, will try and optimize the GIF by storing only the
+    #     rectangular parts of each frame that change with respect to the
+    #     previous. Default False.
+
